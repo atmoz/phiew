ee15c6c0607c025a3c73c8336cb03f4ac69791e4
Adrian Dvergsdal <github.com@konto.atmoz.no>
2010-01-01 21:30:01 +0100

    Replaced tabs with spaces

--- library/Phiew/View.php
+++ library/Phiew/View.php
@@ -13,73 +13,73 @@ namespace Phiew;
  */
 class View
 {
-	protected $_dirname;
-	protected $_data;
-	protected static $_viewHelpers;
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param $dirname string Location for view script folder
-	 */
-	public function __construct($dirname = null)
-	{
-		$this->setDirname($dirname);
-	}
-	
-	/**
-	 * Set folder for view scripts
-	 * 
-	 * @param $dirname string
-	 */
-	public function setDirname($dirname)
-	{
-		$this->_dirname = rtrim($this->_getSafePath($dirname), '/');
-	}
-	
-	/**
-	 * Check input for bad characters
-	 * 
-	 * @param $string string
-	 * @return string
-	 */
-	protected function _getSafePath($string)
-	{
-		$allowedChars = array('/', '.', '-', '_');
-		
-		if (!empty($string) 
-			&& ctype_alnum(str_replace($allowedChars, '', $string))
-			&& strpos($string, '..') === false
-		) {
-			return $string;
-		}
-		else
-		{
-			trigger_error('Bad characters used in path: ' . $string, E_USER_ERROR);
-			return null;
-		}
-	}
-	
-	/**
-	 * Generate full file path for view script
-	 * 
-	 * @param $view string
-	 * @return string
-	 */
-	protected function _getFilename($view)
-	{
-		return $this->_getSafePath($this->_dirname) 
-			   . '/' . $this->_getSafePath($view) . '.phtml';
-	}
-	
-	/**
-	 * Set data
-	 * 
-	 * @param $name string
-	 * @param $value mixed
-	 */
-	public function __set($name, $value)
-	{
+    protected $_dirname;
+    protected $_data;
+    protected static $_viewHelpers;
+    
+    /**
+     * Constructor
+     * 
+     * @param $dirname string Location for view script folder
+     */
+    public function __construct($dirname = null)
+    {
+        $this->setDirname($dirname);
+    }
+    
+    /**
+     * Set folder for view scripts
+     * 
+     * @param $dirname string
+     */
+    public function setDirname($dirname)
+    {
+        $this->_dirname = rtrim($this->_getSafePath($dirname), '/');
+    }
+    
+    /**
+     * Check input for bad characters
+     * 
+     * @param $string string
+     * @return string
+     */
+    protected function _getSafePath($string)
+    {
+        $allowedChars = array('/', '.', '-', '_');
+        
+        if (!empty($string) 
+            && ctype_alnum(str_replace($allowedChars, '', $string))
+            && strpos($string, '..') === false
+        ) {
+            return $string;
+        }
+        else
+        {
+            trigger_error('Bad characters used in path: ' . $string, E_USER_ERROR);
+            return null;
+        }
+    }
+    
+    /**
+     * Generate full file path for view script
+     * 
+     * @param $view string
+     * @return string
+     */
+    protected function _getFilename($view)
+    {
+        return $this->_getSafePath($this->_dirname) 
+               . '/' . $this->_getSafePath($view) . '.phtml';
+    }
+    
+    /**
+     * Set data
+     * 
+     * @param $name string
+     * @param $value mixed
+     */
+    public function __set($name, $value)
+    {
         $this->_data[$name] = $value;
     }
 
@@ -102,57 +102,57 @@ class View
      */
     public function clearData()
     {
-    	$this->_data = array();
+        $this->_data = array();
     }
-	
-	/**
-	 * Output view script
-	 * 
-	 * @param $view string
-	 * @param $data array
-	 */
-	public function render($view, $data = array())
-	{
-		$filename = $this->_getFilename($view);
-		
-		if (is_readable($filename))
-		{
-			foreach ($data as $key => $value)
-			{
-				$this->__set($key, $value);
-			}
+    
+    /**
+     * Output view script
+     * 
+     * @param $view string
+     * @param $data array
+     */
+    public function render($view, $data = array())
+    {
+        $filename = $this->_getFilename($view);
+        
+        if (is_readable($filename))
+        {
+            foreach ($data as $key => $value)
+            {
+                $this->__set($key, $value);
+            }
 
-			include($filename);
-			return true;
-		}
-		else
-		{
-			trigger_error('Kunne ikke finne view: ' . $filename, E_USER_ERROR);
-			return false;
-		}
-	}
-	
-	/**
-	 * Get output from view script as string
-	 * 
-	 * @param $view string
-	 * @param $data array
-	 * @return string
-	 */
-	public function capture($view, $data = array())
-	{
-		ob_start();
-		$this->render($view, $data);
-		return ob_get_clean();
-	}
-	
-	/**
-	 * Escape html characters
-	 *
-	 * @param string $string
-	 */
-	public function escape($string)
-	{
-		return htmlspecialchars($string);
-	}
+            include($filename);
+            return true;
+        }
+        else
+        {
+            trigger_error('Kunne ikke finne view: ' . $filename, E_USER_ERROR);
+            return false;
+        }
+    }
+    
+    /**
+     * Get output from view script as string
+     * 
+     * @param $view string
+     * @param $data array
+     * @return string
+     */
+    public function capture($view, $data = array())
+    {
+        ob_start();
+        $this->render($view, $data);
+        return ob_get_clean();
+    }
+    
+    /**
+     * Escape html characters
+     *
+     * @param string $string
+     */
+    public function escape($string)
+    {
+        return htmlspecialchars($string);
+    }
 }
