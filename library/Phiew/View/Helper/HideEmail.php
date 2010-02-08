ad137c5c199740154be3edcbf31fdd910d1afcc9
Atmoz <github.com@konto.atmoz.no>
2010-02-08 01:36:53 +0100

    Made the mailto link optional in view helper hideEmail()

--- library/Phiew/View/Helper/HideEmail.php
+++ library/Phiew/View/Helper/HideEmail.php
@@ -15,17 +15,32 @@ class Phiew_View_Helper_HideEmail
 	 * @param string $email
 	 * @return string
 	 */
-	public function hideEmail($email) 
+	public function hideEmail($email, $mailto = true) 
 	{ 
 		$character_set = '+-.0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'; 
-		$key = str_shuffle($character_set); $cipher_text = ''; $id = 'e'.rand(1,999999999); 
-		for ($i=0;$i<strlen($email);
-			$i+=1) $cipher_text.= $key[strpos($character_set,$email[$i])];
-		$script = 'var a="'.$key.'";var b=a.split("").sort().join("");var c="'.$cipher_text.'";var d="";'; 
-		$script.= 'for(var e=0;e<c.length;e++)d+=b.charAt(a.indexOf(c.charAt(e)));'; 
-		$script.= 'document.getElementById("'.$id.'").innerHTML="<a href=\\"mailto:"+d+"\\">"+d+"</a>"'; 
+		$key = str_shuffle($character_set); 
+		$cipher_text = ''; 
+		$id = 'e' . rand(1, 999999999); 
+		
+		for ($i = 0; $i < strlen($email); $i += 1)
+		{
+			$cipher_text .= $key[strpos($character_set, $email[$i])];
+		}
+		
+		if ($mailto)
+		{
+			$innerHTML = '"<a href=\\"mailto:"+d+"\\">"+d+"</a>"';
+		}
+		else
+		{
+			$innerHTML = 'd';
+		}
+		
+		$script = 'var a="'.$key.'";var b=a.split("").sort().join("");var c="'.$cipher_text.'";var d="";'
+		        . 'for(var e=0;e<c.length;e++)d+=b.charAt(a.indexOf(c.charAt(e)));'
+		        . 'document.getElementById("'.$id.'").innerHTML=' . $innerHTML;
 		$script = "eval(\"".str_replace(array("\\",'"'),array("\\\\",'\"'), $script)."\")"; 
 		$script = '<script type="text/javascript">/*<![CDATA[*/'.$script.'/*]]>*/</script>'; 
-		return '<span id="'.$id.'">[javascript protected email address]</span>'.$script; 
+		return '<span id="'.$id.'">[javascript protected email address]</span>' . $script; 
 	}
 }
