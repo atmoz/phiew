cf8d4070ac74f787b2065af3a0bf25c0e4d0b709
Atmoz <github.com@konto.atmoz.no>
2010-02-17 00:59:28 +0100

    Adding Phiew_Autoload

---
+++ library/Phiew/Autoload.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Adrian Dvergsdal
+ * @link http://github.com/atmoz/phiew
+ * @license http://creativecommons.org/licenses/by-sa/3.0/us/
+ */
+
+/**
+ * Autoload Phiew classes
+ */
+class Phiew_Autoload
+{
+	/**
+	 * Adds this class to the SPL autoload stack
+	 */
+	public static function register()
+	{
+		spl_autoload_register('Phiew_Autoload::loadClass');
+	}
+
+	/**
+	 * Loads the required class
+	 *
+	 * @param string $className
+	 */
+	public static function loadClass($className)
+	{
+		$phiewFolder = dirname(__FILE__) . '/../';
+		$classPath   = str_replace('_',  '/', $className) . '.php';
+		$file        = $phiewFolder . $classPath;
+
+		if (is_readable($file))
+		{
+			require $file;
+		}
+	}
+}
