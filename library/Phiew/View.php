2b72aff1c8e387fa38bc81a2bb03ebaa3c0af613
Atmoz <github.com@konto.atmoz.no>
2010-03-06 04:02:09 +0100

    PHPDocs updated for v0.12.0

--- library/Phiew/View.php
+++ library/Phiew/View.php
@@ -1,5 +1,9 @@
 <?php
 /**
+ * Phiew
+ * - Simple and fast MVC components for PHP 5
+ *
+ * @version 0.12.0
  * @author Adrian Dvergsdal
  * @link http://github.com/atmoz/phiew
  * @license http://creativecommons.org/licenses/by-sa/3.0/us/
