a1ff9b76431ea498e85af9ecb296116037afc1fe
Atmoz <github.com@konto.atmoz.no>
2010-03-17 20:22:59 +0100

    Phiew_Controller enters the battlefield

---
+++ library/Phiew/Controller.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Phiew
+ * - Simple and fast MVC components for PHP 5
+ *
+ * @version 0.12.0
+ * @author Adrian Dvergsdal
+ * @link http://github.com/atmoz/phiew
+ * @license http://creativecommons.org/licenses/by-sa/3.0/us/
+ */
+
+/**
+ * Makes your controllers easier to use
+ */
+class Phiew_Controller
+{
+	/**
+	 * Render template
+	 *
+	 * @param string $view
+	 * @param array $data
+	 * @return string|null
+	 */
+	protected function _render($view, $data = array())
+	{
+		return Phiew_View::render($view, $data);
+	}
+
+	/**
+	 * Render template based on controller action. Using class name as folder.
+	 *
+	 * @param array $data
+	 * @return string|null
+	 */
+	protected function _renderAction($action, $data = array())
+	{
+		return $this->_render(get_class($this) . '/' . $action, $data);
+	}
+
+	/**
+	 * Render template based on current controller action (calling function)
+	 *
+	 * @param array $data
+	 * @return string|null
+	 */
+	protected function _renderCurrentAction($data = array())
+	{
+		// Get calling function name
+		$trace = debug_backtrace();
+		$function = $trace[1]['function'];
+
+		return $this->_renderAction($function, $data);
+	}
+}
