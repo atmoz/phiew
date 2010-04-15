10dea33214a69890618698c7c8839fce190195ab
Atmoz <github.com@konto.atmoz.no>
2010-04-15 02:13:02 +0200

    Added Phiew_TypeHint_Collection

---
+++ library/Phiew/TypeHint/WrongTypeException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Phiew
+ * - Simple and fast MVC components for PHP 5
+ *
+ * @version 0.12.0
+ * @author Adrian Dvergsdal
+ * @link http://github.com/atmoz/phiew
+ * @license http://creativecommons.org/licenses/by-sa/3.0/us/
+ */
+
+/**
+ * Type hint exception
+ */
+class Phiew_TypeHint_WrongTypeException extends Exception
+{
+	// Empty body. Only need the unique exception name
+}
